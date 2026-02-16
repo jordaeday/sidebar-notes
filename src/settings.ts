99d229ab14f55bfd80c549ed947f90fc72540fa6
Jordan Day <jordaeday22@gmail.com>
2026-02-16 16:43:40 -0600

    changed relevant names

--- src/settings.ts
+++ src/settings.ts
@@ -1,35 +1,33 @@
 import {App, PluginSettingTab, Setting} from "obsidian";
-import MyPlugin from "./main";
+import SidebarNotesPlugin from "./main";
 
-export interface MyPluginSettings {
-	mySetting: string;
+export interface SidebarNotesSettings {
+	autoOpenSidebar: boolean;
 }
 
-export const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+export const DEFAULT_SETTINGS: SidebarNotesSettings = {
+	autoOpenSidebar: true,
+};
 
-export class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+export class SidebarNotesSettingTab extends PluginSettingTab {
+	plugin: SidebarNotesPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: SidebarNotesPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
 		const {containerEl} = this;
-
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Settings #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+			.setName('Open sidebar automatically')
+			.setDesc('Open the side note panel whenever a Markdown note is focused (desktop only).')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.autoOpenSidebar)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.autoOpenSidebar = value;
 					await this.plugin.saveSettings();
 				}));
 	}
